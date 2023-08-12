063bc4ae38d959f991870f4d27c49aa8df17c52f
Ian_França <iiann_2009@hotmail.com>
2023-08-12 12:03:29 -0300

    corrige o texto

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { ImagemNossa } from "../public/images/fotoNossa.png";
 function Home() {
   return (
     <div style={{ backgroundImage: "url(${ImageNossa})" }}>
-      <h1>Oi, neném! Se você me ama, me manda dá um beijinho? kkkkkkk</h1>
+      <h1>Oi, neném! Se você me ama, me dá um beijinho? kkkkkkk</h1>
       <img src="/images/fotoNossa.png" alt="foto nossa" />
     </div>
   );
