50e4ee8b0ce11e6c28bfa77f4f7d35d79d0e82a7
Ian_França <iiann_2009@hotmail.com>
2023-08-12 09:20:56 -0300

    cria a pasta public e a pasta images com a imagem fotoNossa.png que foi carregada no home

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// import ImageNossa from "./ImagemNossa";
+import { ImagemNossa } from "../public/images/fotoNossa.png";
+
+function Home() {
+  return (
+    <div style={{ backgroundImage: "url(${ImageNossa})" }}>
+      <h1>Oi, neném! Se você me ama, me manda dá um beijinho? kkkkkkk</h1>
+      <img src="/images/fotoNossa.png" alt="foto nossa" />
+    </div>
+  );
+}
+export default Home;
